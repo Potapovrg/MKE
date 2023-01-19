d80d0539b4c1861569a98c204d4c7bbdac07c585
Roman <potapovrg@gmail.com>
2023-01-19 15:19:04 +0300

    USB composite device works

---
+++ Debug/Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usb_device.c \
+../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usbd_desc.c 
+
+OBJS += \
+./Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usb_device.o \
+./Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usbd_desc.o 
+
+C_DEPS += \
+./Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usb_device.d \
+./Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/usbd_desc.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/%.o: ../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/%.c Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Composite -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/COMPOSITE/Inc -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/HID_KEYBOARD/Inc -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Core/Inc -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Target -I../Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/HID_MOUSE/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
