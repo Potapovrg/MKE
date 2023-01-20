776d8d2999e613725fdb4bf53c4054e57c153a8f
Roman <potapovrg@gmail.com>
2023-01-20 16:07:22 +0300

    Before SPI Setup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -114,27 +114,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  mouse_keyboard_test();
     /* USER CODE END WHILE */
-	  //mousehid.mouse_x=10;
-	  for (int y=0; y<4; y++)
-	  {
-
-		  for (int i=0; i < 30; i++)
-		  {
-			  HAL_Delay(50);
-			  mousehid=mousemove[y];
-			  USBD_HID_Mouse_SendReport(&hUsbDevice, &mousehid, sizeof (mousehid));
-		  }
-		  USBD_HID_Keybaord_SendReport(&hUsbDevice, &kbdPress, 8);
-		  HAL_Delay(50);
-		  USBD_HID_Keybaord_SendReport(&hUsbDevice, &kbdRelease, 8);
-	  }
-
-	  //USBD_HID_Keybaord_SendReport(&hUsbDevice, kbdPress, 8);
-
-	  //USBD_HID_Keybaord_SendReport(&hUsbDevice, hialex, sizeof(hialex));
-    /* USER CODE BEGIN 3 */
   }
+
+    /* USER CODE BEGIN 3 */
   /* USER CODE END 3 */
 }
 
@@ -184,7 +168,22 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void mouse_keyboard_test(void)
+{
+	  for (int y=0; y<4; y++)
+	 	  {
 
+	 		  for (int i=0; i < 30; i++)
+	 		  {
+	 			  HAL_Delay(50);
+	 			  mousehid=mousemove[y];
+	 			  USBD_HID_Mouse_SendReport(&hUsbDevice, &mousehid, sizeof (mousehid));
+	 		  }
+	 		  USBD_HID_Keybaord_SendReport(&hUsbDevice, &kbdPress, 8);
+	 		  HAL_Delay(50);
+	 		  USBD_HID_Keybaord_SendReport(&hUsbDevice, &kbdRelease, 8);
+	 	  }
+}
 /* USER CODE END 4 */
 
 /**
